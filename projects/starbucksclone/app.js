3152c79a205cee8086887f4f8d1012a751b1b10c
Kristie Patterson <kpatterson395@gmail.com>
2018-06-27 12:00:37 -0400

    update dropdown menu for mobile versions

--- projects/starbucksclone/app.js
+++ projects/starbucksclone/app.js
@@ -1,6 +1,12 @@
 
+var navDisplay = false;
 
 $(document).ready(function() {
+
+	  $(".ctrlNav").on("click", function(){
+	  	displayNav();
+
+	  });
 
 
 	$(".menu-head").on("mouseenter", function(){
@@ -22,4 +28,21 @@ function displayMenu(className) {
 function hideMenu(className) {
 		$(".menu-dropdown."+className).css("opacity", "0");
 		$(".menu-dropdown."+className).css("visiblity", "hidden");
+}
+
+function displayNav() {
+	if (!navDisplay){
+		$(".navBar-links").css("display", "block");
+	  	$(".fa-bars").css("display", "none");
+	  	$(".fa-times").css("display", "block");
+	  	navDisplay= true;
+	}
+	else {
+		$(".navBar-links").css("display", "none");
+	  	$(".fa-bars").css("display", "block");
+	  	$(".fa-times").css("display", "none");
+	  	navDisplay = false;	
+	}
+
+
 }
