f8b5e1e003ca96ec02f9f0561a2cb9b7e56091c1
Kristie Patterson <kpatterson395@gmail.com>
2018-07-13 11:14:47 -0400

    update header and div sizing for mobile versions

--- projects/starbucksclone/app.js
+++ projects/starbucksclone/app.js
@@ -2,20 +2,30 @@
 var navDisplay = false;
 
 $(document).ready(function() {
+	$(window).on("resize", function(){
+		if($(this).width() > 768){
+			$(".navBar-links").css("display", "inline-block");
+			$(".fas").css("display", "none");
+			navDis=false;
+		}
+		else if ($(this).width()<768){
+			$(".navBar-links").css("display", "none");
+			$(".fa-bars").css("display", "block");
+			$(".fa-times").css("display", "none");
+			navDis=false;			
+		}
+	});
 
-	  $(".ctrlNav").on("click", function(){
-	  	displayNav();
-
-	  });
-
-
+	$(".ctrlNav").on("click", function(){
+		displayNav();
+	});
 	$(".menu-head").on("mouseenter", function(){
 		displayMenu($(this).attr("id"));
-	})
+	});
 
 	$(".menu-head").on("mouseleave", function(){
 		hideMenu($(this).attr("id"));
-	})
+	});
 });
 
 
@@ -35,6 +45,7 @@ function displayNav() {
 		$(".navBar-links").css("display", "block");
 	  	$(".fa-bars").css("display", "none");
 	  	$(".fa-times").css("display", "block");
+	  	
 	  	navDisplay= true;
 	}
 	else {
