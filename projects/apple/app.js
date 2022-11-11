8557829aa187339dd64701672795389c13971836
Kristie Patterson <kpatterson395@gmail.com>
2022-11-11 11:28:55 -0500

    responsive

--- projects/apple/app.js
+++ projects/apple/app.js
@@ -5,9 +5,32 @@ let counters = [".counter0", ".counter1", ".counter2", ".counter3", ".counter4",
 
 let focalSlide = 0
 let currentCounter = ".counter0"
+let width = $(slides[0]).width() + 5
+
+
+$.fn.isOnScreen = function () {
+
+    var win = $(window);
+
+    var viewport = {
+        top: win.scrollTop(),
+        left: win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
+
+
 
 function updateSlides() {
-    let position = ((985 * .75) + (985 * (focalSlide))) * -1
+    let position = ((width * .75) + (width * (focalSlide))) * -1
     $(slides[focalSlide]).toggleClass("hide")
     $(".slideShow").css({ left: `${position}px` })
     slideText = $(slides[focalSlide]).children()[0]
@@ -91,5 +114,31 @@ $(document).ready(function () {
     $(".columnTitle").on("click", function () {
         $(this).next().toggleClass("showItem")
     })
+    // $(window).scroll(function () {
+    //     if ($(".slideShowContainer").isOnScreen()) {
+
+    //     }
+    // })
+    // autoSlides()
 
 })
+
+function autoSlides() {
+    updateCounterColor("grey")
+
+    hideSlide()
+    if (focalSlide = 10) {
+        focalSlide = 0
+    } else {
+        focalSlide = focalSlide + 1
+    }
+    updateSlides()
+    if (currentCounter === counters[10]) {
+        currentCounter = counters[0]
+    } else {
+        currentCounter = counters[focalSlide]
+    }
+    updateCounterColor("black")
+    // console.log('ahhh')
+    setTimeout(autoSlides, 2000)
+}
